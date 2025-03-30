b182f121a34a7d64c7370d678d3365c74207c4dc
Bruno C <bruno.ferreira5@estudante.ufla.br>
2025-03-29 22:03:03 -0300

    Alterando o Caminho do CPLEX p/ o Original (por precaução)

--- run_challenge.py
+++ run_challenge.py
@@ -5,7 +5,7 @@ import platform
 
 # Paths to the libraries (may be differ)
 # default installation path for Linux: /opt/ibm/ILOG/CPLEX_Studio2211/opl/bin/x86-64_linux
-CPLEX_PATH = "/opt/ibm/ILOG/CPLEX_Studio2211/opl/bin/x86-64_linux"    # original value: $HOME/CPLEX_Studio2211/opl/bin/arm64_osx/
+CPLEX_PATH = "$HOME/CPLEX_Studio2211/opl/bin/arm64_osx/"
 OR_TOOLS_PATH = "$HOME/Documents/or-tools/build/lib/"
 
 USE_CPLEX = True
